fa21753f2a6388e7e23555ebd1645cb2200ce4d3
Taha KAMIL <itaha.kamil@gmail.com>
2022-08-06 00:55:09 +0100

    fix prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 import { environment as common } from './environment';
 
 export const environment = {
-  ...common,
-  production: true
+  production: true,
+  weatherApiBaseUrl:
+    'https://api.openweathermap.org/data/2.5/weather',
+  Key: '8ba1c009f0b27ebab06d8547f1437a22',
 };
