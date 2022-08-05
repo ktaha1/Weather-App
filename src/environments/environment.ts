c6b3484b489ee57e1f08d76089a7bf9c14119a38
Taha KAMIL <itaha.kamil@gmail.com>
2022-08-05 23:41:17 +0100

    fix env properties for prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
@@ -8,12 +5,3 @@ export const environment = {
     'https://api.openweathermap.org/data/2.5/weather',
   Key: '8ba1c009f0b27ebab06d8547f1437a22',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
